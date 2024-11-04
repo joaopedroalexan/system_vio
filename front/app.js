143c92e4c555136e0ba47f6dfbfa32d7348a4c5c
joaopedroalexan <joaopedroalexsilva@gmail.com>
2024-11-04 14:43:18 -0300

    new

--- front/app.js
+++ front/app.js
@@ -1,22 +1,22 @@
 // Chamada da função "createUser" para associação ao evento de envio do formulário
-document
-  .getElementById("formulario-registro")
-  .addEventListener("submit", createUser);
+document.getElementById("formulario_registro").addEventListener("submit", createUser);
 
 document.addEventListener("DOMContentLoaded", getAllUsers);
+
+document.addEventListener("DOMContentLoaded", getAllUsersTable);
 
 function createUser(event) {
   // Previne o comportamento padrão do formulário, ou seja, impede que ele seja enviado e recarregue a página
   event.preventDefault();
 
   // Captura os valores dos campos do formulário
-  const name = document.getElementById("nome").value;
+  const name = document.getElementById("name").value;
   const cpf = document.getElementById("cpf").value;
   const email = document.getElementById("email").value;
-  const password = document.getElementById("senha").value;
+  const password = document.getElementById("password").value;
 
   // Requisição HTTP para o endpoint de cadastro de usuário
-  fetch("http://10.89.240.105:5000/api/v1/user/", {
+  fetch("http://10.89.240.3:5000/api/v1/user/", {
     // Realiza uma chamada HTTP para o servidor (a rota definida)
     method: "POST",
     headers: {
@@ -62,7 +62,7 @@ function createUser(event) {
 }
 
 function getAllUsers() {
-  fetch("http://10.89.240.105:5000/api/v1/user/", {
+  fetch("http://10.89.240.3:5000/api/v1/user/", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -71,10 +71,11 @@ function getAllUsers() {
     .then((response) => {
       if (response.ok) {
         return response.json();
-      }
+      }else{
       return response.json().then((err) => {
         throw new Error(err.error);
       });
+    }
     })
     .then((data) => {
       const userList = document.getElementById("user-list");
@@ -82,12 +83,59 @@ function getAllUsers() {
 
       data.users.forEach((user) => {
         const listItem = document.createElement("ul");
-        listItem.textContent = `Nome: ${user.name}, CPF: ${user.cpf}, Email: ${user.email}`;
+        listItem.textContent = `Name: ${user.name}, CPF: ${user.cpf}, Email: ${user.email}`;
         userList.appendChild(listItem);
       });
     })
     .catch((error) => {
       alert("Erro ao obter usuários" + error.message);
       console.error("Erro: ", error.message);
+    });
+}
+
+function getAllUsersTable() {
+  fetch("http://10.89.240.3:5000/api/v1/user/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }else{
+      return response.json().then((err) => {
+        throw new Error(err.error);
+      });
+    }
+    })
+    .then((data) => {
+      const userList = document.getElementById("user-list-tabela");
+      // limpa a lista antes de adicionar novos itens
+      userList.innerHTML = "";
+      // VERIFICA SE HÁ USUARIOS RETORNADOS E OS ADICIONA A TABELA
+      data.users.forEach((user) => {
+        // CRIA UMA NOVA LINHA E POVOA-LA
+        const tr = document.createElement("tr");
+
+        //cria celulas para nome, cpf, email
+        const tdCPF = document.createElement("td");
+        tdCPF.textContent = user.cpf;
+        tr.appendChild(tdCPF);
+        
+                const tdName = document.createElement("td");
+        tdName.textContent = user.name;
+        tr.appendChild(tdName);
+
+        const tdEmail = document.createElement("td");
+        tdEmail.textContent = user.email;
+        tr.appendChild(tdEmail);
+
+        userList.appendChild(tr);
+      });
+    })
+    .catch((error) => {
+      alert("Erro ao obter usuarios: " + error.message);
+      console.error("Erro:", error.message);
     });
 }
